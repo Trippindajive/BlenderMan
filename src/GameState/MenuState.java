706938e114b04523ce9c4cd3c0b09852ad611fba
ashaunthomas <ashaunthomas@siu.edu>
2020-05-25 07:49:20 -0500

    sets starting screen back to menu

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -62,7 +62,7 @@ public class MenuState extends GameState {
 	private void select() {
 		if(currentChoice == 0) {
 			// Start
-			gsm.setState(STATE.LEVEL_ONE);
+			gsm.setState(STATE.MENU);
 		}
 		if(currentChoice == 1) {
 			// Help
